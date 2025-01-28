f7e6abab815921b2b834b5cc1cabb1634ef84dd9
Aimar <ikcdc@plaiaundi.net>
2025-01-28 09:19:35 +0100

    Clase Reserva finalizada y creacion Clase Sala

--- src/model/Reserva.java
+++ src/model/Reserva.java
@@ -7,5 +7,58 @@ public class Reserva {
 	private int ID;
 	private Date fecIni;
 	private Date fecFin;
+	private Cliente cliente;
+    private Sala sala;
+	
+	//CONSTRUCTOR
+    public Reserva(int iD, Date fecIni, Date fecFin, Cliente cliente, Sala sala) {
+		super();
+		ID = iD;
+		this.fecIni = fecIni;
+		this.fecFin = fecFin;
+		this.cliente = cliente;
+		this.sala = sala;
+	}
+    
+	//GETTER & SETTERS
+    public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public Date getFecIni() {
+		return fecIni;
+	}
+
+	public void setFecIni(Date fecIni) {
+		this.fecIni = fecIni;
+	}
+
+	public Date getFecFin() {
+		return fecFin;
+	}
+
+	public void setFecFin(Date fecFin) {
+		this.fecFin = fecFin;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
 	
 }
