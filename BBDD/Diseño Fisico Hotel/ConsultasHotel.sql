2f2a500dfd5d6ca26111edfd94478d1fa8a49c87
Pablo Fernandez Alonso <ikdhb@plaiaundi.net>
2025-01-29 08:05:59 +0100

    Consultas BBDD

--- BBDD/Diseño Fisico Hotel/ConsultasHotel.sql
+++ BBDD/Diseño Fisico Hotel/ConsultasHotel.sql
@@ -1,7 +1,9 @@
 select * from cliente;
 select * from reserva;
 select * from sala natural join habitacion;
-select nom "Hotel", sum(capacidad) "Capacidad" from hotel join sala using(id) natural join habitacion group by nom;
+select nom "Hotel", sum(capacidad) "Capacidad" from hotel join sala using(id) natural join habitacion where id = ? group by nom;
 select * from sala natural join habitacion;
 select * from sala natural join salareuniones;
 select * from espacioscomunes;
+INSERT INTO Cliente (DNI, nom, ape, tlfno, email, btrabajador, tarifa, pass) VALUES
+(?, ?, ?, ?, ?, FALSE, ?, SHA2(?, 256)),
